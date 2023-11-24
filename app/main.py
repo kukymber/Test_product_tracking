13ba739063244c6141a1fe727eeb4b6da5cdff90
kykumber <ostap_to@mail.ru>
2023-11-24 18:06:26 +0800

    setting main.py and schemas.py

--- app/main.py
+++ app/main.py
@@ -1,17 +1,21 @@
 from fastapi import FastAPI
 from app.database import engine
 from app.models.models import Base
-import app.endpoints
+import app.endpoints.components as components
+import app.endpoints.semiproducts as semiproducts
+import app.endpoints.products as products
+import app.endpoints.orders as orders
+import app.endpoints.invoices as invoices
 
 app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
 
 app.include_router(components.router, prefix="/api/v1", tags=["components"])
-app.include_router(semiproducts.router, prefix="/api/v1", tags=["semiproducts"])
-app.include_router(products.router, prefix="/api/v1", tags=["products"])
-app.include_router(orders.router, prefix="/api/v1", tags=["orders"])
-app.include_router(invoices.router, prefix="/api/v1", tags=["invoices"])
+# app.include_router(semiproducts.router, prefix="/api/v1", tags=["semiproducts"])
+# app.include_router(products.router, prefix="/api/v1", tags=["products"])
+# app.include_router(orders.router, prefix="/api/v1", tags=["orders"])
+# app.include_router(invoices.router, prefix="/api/v1", tags=["invoices"])
 
 
 @app.get("/")
--- app/schemas/schemas.py
+++ app/schemas/schemas.py
@@ -18,4 +18,4 @@ class Component(ComponentBase):
     id: int
 
     class Config:
-        orm_mode = True
+        from_attributes = True
