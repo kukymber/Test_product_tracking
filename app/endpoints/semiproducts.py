41b012fbfd76735dfc7ddb07216b9e662bf405a7
kykumber <ostap_to@mail.ru>
2023-11-25 20:59:28 +0800

    changed imports db in the router semiproducts.py and

--- app/endpoints/semiproducts.py
+++ app/endpoints/semiproducts.py
@@ -2,30 +2,28 @@ from fastapi import APIRouter, HTTPException, Depends
 from sqlalchemy.orm import Session
 from typing import List
 from app.database import get_db
-from app.models.models import SemiProduct, Component
-from app.schemas.semiproduct import SemiProductCreate, SemiProductUpdate, SemiProductBase, SemiProduct
+from app.models.models import SemiProduct as DbSemiProduct, Component as DbComponent
+from app.schemas.semiproduct import SemiProductCreate, SemiProductUpdate, SemiProduct, SemiProductBase
 
 router = APIRouter()
 
 
-@router.get("/semiproducts/", response_model=List[SemiProductBase])
+# Используем модель Pydantic для ответа
+@router.get("/semiproducts/", response_model=List[SemiProduct])
 def read_semiproducts(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     try:
-        semiproducts = db.query(SemiProduct).offset(skip).limit(limit).all()
+        semiproducts = db.query(DbSemiProduct).offset(skip).limit(limit).all()
         return semiproducts
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
 
-from app.models.models import SemiProduct, Component
-
-
-@router.post("/semiproducts/", response_model=SemiProductCreate)
+@router.post("/semiproducts/", response_model=SemiProduct)
 def create_semiproduct(semiproduct: SemiProductCreate, db: Session = Depends(get_db)):
-    new_semiproduct = SemiProduct(name=semiproduct.name)
+    new_semiproduct = DbSemiProduct(name=semiproduct.name)
 
     if semiproduct.component_ids:
-        components = db.query(Component).filter(Component.id.in_(semiproduct.component_ids)).all()
+        components = db.query(DbComponent).filter(DbComponent.id.in_(semiproduct.component_ids)).all()
         new_semiproduct.components = components
 
     db.add(new_semiproduct)
@@ -38,27 +36,27 @@ def create_semiproduct(semiproduct: SemiProductCreate, db: Session = Depends(get
         raise HTTPException(status_code=500, detail=str(e))
 
 
-@router.put("/semiproducts/{semiproduct_id}", response_model=SemiProductUpdate)
+@router.put("/semiproducts/{semiproduct_id}", response_model=SemiProduct)
 def update_semiproduct(semiproduct_id: int, semiproduct: SemiProductUpdate, db: Session = Depends(get_db)):
-    db_semiproduct = db.query(SemiProduct).filter(SemiProduct.id == semiproduct_id).first()
+    db_semiproduct = db.query(DbSemiProduct).filter(DbSemiProduct.id == semiproduct_id).first()
     if db_semiproduct is None:
         raise HTTPException(status_code=404, detail="SemiProduct not found")
 
-    if semiproduct.name is not None:
+    if semiproduct.name:
         db_semiproduct.name = semiproduct.name
 
     if semiproduct.component_ids is not None:
         current_component_ids = {component.id for component in db_semiproduct.components}
         new_component_ids = set(semiproduct.component_ids)
 
-        for component in db_semiproduct.components:
-            if component.id not in new_component_ids:
-                db_semiproduct.components.remove(component)
-
+        # Обновление списка компонентов
         for component_id in new_component_ids - current_component_ids:
-            component = db.query(Component).filter(Component.id == component_id).first()
+            component = db.query(DbComponent).filter(DbComponent.id == component_id).first()
             if component:
                 db_semiproduct.components.append(component)
+
+        db_semiproduct.components = [component for component in db_semiproduct.components if
+                                     component.id in new_component_ids]
 
     try:
         db.commit()
@@ -69,10 +67,9 @@ def update_semiproduct(semiproduct_id: int, semiproduct: SemiProductUpdate, db: 
         raise HTTPException(status_code=500, detail=str(e))
 
 
-
 @router.delete("/semiproducts/{semiproduct_id}", response_model=SemiProductBase)
 def delete_semiproduct(semiproduct_id: int, db: Session = Depends(get_db)):
-    db_semiproduct = db.query(SemiProduct).filter(SemiProduct.id == semiproduct_id).first()
+    db_semiproduct = db.query(DbSemiProduct).filter(DbSemiProduct.id == semiproduct_id).first()
     if db_semiproduct is None:
         raise HTTPException(status_code=404, detail="SemiProduct not found")
     try:
